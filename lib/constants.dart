7562a09761a472d319296f93e1ce56375ba74397
Huda Saylam <107221918+hudasaylam@users.noreply.github.com>
2025-07-13 00:16:07 +0300

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color.fromARGB(255, 238, 232, 203);
+const kPrimaryColor = Color.fromARGB(115, 216, 250, 81);
+const kSecondaryColor = Colors.amber;
+const kTextColor = Color.fromARGB(255, 48, 255, 107);
+const kTextLightColor = Colors.grey;
+const kBlueColor = Colors.blueGrey;
+const kDefaultColor = Color.fromARGB(255, 79, 188, 83);
+const kDefaultPadding = 20.0;
